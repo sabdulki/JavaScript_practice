566a1ac8215c48c0f2e53b6962b5b1abc098f554
Sofya <abdulkina.s328@gmail.com>
2024-03-05 22:30:39 +0400

    better button using js

--- assets/JS/learn_html.js
+++ assets/JS/learn_html.js
@@ -1,6 +1,20 @@
-function catFlower()
-{
-	document.getElementById("demo").innerHTML = "No, this page is about flowers!";
-	// document.getElementById("demo").style.backgroundColor = "green";
-}
 
+let answerIsVisible = false
+
+function catFlower(){
+
+	let demo = document.getElementById("demo") 
+	let button = document.getElementById("button-demo")
+
+	if (answerIsVisible) {
+		demo.innerHTML = "So, what is this page about? Maybe about cats?"
+		demo.style.backgroundColor = null
+		button.textContent = "Are you sure?"
+	} else {
+		demo.innerHTML = "No, this page is about flowers!";
+		demo.style.backgroundColor = "lightgreen";
+		button.textContent = "Try again!"
+	}
+	answerIsVisible = !answerIsVisible
+	// document.getElementById("demo").style.color = "white";
+}
