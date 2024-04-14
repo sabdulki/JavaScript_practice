34190130763bbdf271900874bf7b69363b06c40a
Sofya <abdulkina.s328@gmail.com>
2024-04-14 19:45:44 +0400

    started equation

--- assets/JS/calculator.js
+++ assets/JS/calculator.js
@@ -9,24 +9,59 @@ let sign;
 let number;
 let tempSign1;
 let tempSign2;
+
+function isSign(char) //return true or false
+{
+	return ((char == "+" || char == "-" || char == "*" || char == "/" ));
+}
+
 function createString(input) {
 	// if (Number(input) == NaN)
 	// 	sign = input;
 	// else
 	// 	number = input;
-	str += input;
-	if (str.length > 1)
+	let i = 0;
+	if (input == "=")
+		createEquation(str);
+	if (isSign(input) && isSign(str[str.length-1]))
 	{
-		for (let i = 1; i < str.length; i++)
-		{
-			tempSign1 = Number(str[i]);
-			tempSign2 = Number(str[i+1]);
-			if (isNaN(tempSign1) && isNaN(tempSign2)) // && str[i] != str[i-1])
-				str = str.substring(0, i) + str[i + 1] + str.substring(i + 1);
-		}
+		str = str.slice(0, str.length - 1)
 	}
-	
+	str += input;
 	updateResult(str);
+}
+
+function createEquation(str)
+{
+	createNum(str);
+	for (let i = 0; i < str.length, i++;)
+	{
+		// if (isSign(str[i]))
+		// 	sign = str[i];
+		if (Number(str[i]))
+			i++;
+
+	}
+	num
+}
+/*
+запустить loop который будет проходиться по всей строке и искать числа в ней.
+после нахождения искать signs и выполнять операции.
+*/
+function createNum(str)
+{
+	let num;
+	for (let i = 0; i < str.length, i++;)
+	{
+		// if (isSign(str[i]))
+		// 	sign = str[i];
+		if (Number(str[i]))
+			i++;
+
+	}
+	num = Number(str.substring(0, i));
+	console.log(num);
+	return(num, i);
 }
 
 function clearString() {
